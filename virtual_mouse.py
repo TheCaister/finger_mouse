d826695e8cddcecc3ac912f0186ef71a9d1922f1
Edison Cai <edisoncai2001@gmail.com>
2021-07-04 18:13:51 +0100

    fixing some indent and argument errors

--- virtual_mouse.py
+++ virtual_mouse.py
@@ -30,7 +30,7 @@ width_screen, height_screen = autopy.screen.size()
 while True:
     # Reading webcam
     success, img = cap.read()
-    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    # img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
     img = detector.find_hands(img)
 
     # Getting landmarks and bounding box coordinates
@@ -41,44 +41,40 @@ while True:
         x1, y1 = landmarks_list[8][1:]
         x2, y2 = landmarks_list[12][1:]
 
-    # Get list of fingers that are up/down
-    fingers = detector.fingers_up()
+        # Get list of fingers that are up/down
+        fingers = detector.fingers_up()
 
-    # Drawing a region where you can move your fingers
-    cv2.rectangle(img, (frame_reduction, frame_reduction),
-                  (width_cam, frame_reduction, height_cam - frame_reduction),
-                  (255, 0, 255), 2)
-
-    # If index finger is up and middle finger is down
-    if fingers[1] == 1 and fingers[2] == 0:
+        # Drawing a region where you can move your fingers
         cv2.rectangle(img, (frame_reduction, frame_reduction),
-                      (width_cam, frame_reduction, height_cam - frame_reduction),
+                      (width_cam - frame_reduction, height_cam - frame_reduction),
                       (255, 0, 255), 2)
-        # Interpolate fingertip coordinates from webcam to monitor screen, then moving the mouse
-        x3 = np.interp(x1, (frame_reduction, width_cam - frame_reduction), (0, width_screen))
-        y3 = np.interp(x1, (frame_reduction, height_cam - frame_reduction), (0, height_screen))
 
-        # Smoothening the mouse by
-        # Adding the previous location to the difference between the interpolated location and
-        # the previous location divided by the smoothening value
-        # The cursor decelerates towards the intended position for a smooth effect
-        current_location_x = previous_location_x + (x3 - previous_location_x) / smoothening
-        current_location_y = previous_location_y + (y3 - previous_location_y) / smoothening
+        # If index finger is up and middle finger is down
+        if fingers[1] == 1 and fingers[2] == 0:
+            # Interpolate fingertip coordinates from webcam to monitor screen, then moving the mouse
+            x3 = np.interp(x1, (frame_reduction, width_cam - frame_reduction), (0, width_screen))
+            y3 = np.interp(y1, (frame_reduction, height_cam - frame_reduction), (0, height_screen))
 
-        # Inverting the x direction for more intuitive control
-        autopy.mouse.move(width_screen - current_location_x, current_location_y)
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        previous_location_x, previous_location_y = current_location_x, current_location_y
+            # Smoothening the mouse by
+            # Adding the previous location to the difference between the interpolated location and
+            # the previous location divided by the smoothening value
+            # The cursor decelerates towards the intended position for a smooth effect
+            current_location_x = previous_location_x + (x3 - previous_location_x) / smoothening
+            current_location_y = previous_location_y + (y3 - previous_location_y) / smoothening
 
-    # If index finger and middle finger are up
-    if fingers[1] == 1 and fingers[2] == 1:
-        # Getting distance between the 2 fingertips
-        length, img, line_info = detector.find_distance(8, 12, img)
+            autopy.mouse.move(current_location_x, current_location_y)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            previous_location_x, previous_location_y = current_location_x, current_location_y
 
-        # If the 2 tips are close enough, draw a green circle between then
-        if length < 40:
-            cv2.circle(img, (line_info[4], line_info[5]), 15, (0, 255, 0), cv2.FILLED)
-            autopy.mouse.click()
+        # If index finger and middle finger are up
+        if fingers[1] == 1 and fingers[2] == 1:
+            # Getting distance between the 2 fingertips
+            length, img, line_info = detector.find_distance(8, 12, img)
+
+            # If the 2 tips are close enough, draw a green circle between then
+            if length < 40:
+                cv2.circle(img, (line_info[4], line_info[5]), 15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click()
 
     # Displaying FPS
     current_time = time.time()
